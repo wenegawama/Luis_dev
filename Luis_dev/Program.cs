cf3cea0d43ece16431bdfcfb11923851240338cd
Wenega Wama <narcissewatherloy@gmail.com>
2022-06-05 19:36:18 -0300

    Add project files.

---
+++ Luis_dev/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luis_dev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var valores = Console.ReadLine();
+            //var valoresArray = valores.Split(' ');
+            //for (var i = 0; i < valoresArray.Length; i++)
+            //{
+            //    Console.WriteLine(valoresArray[i]);
+            //}
+
+            //var contador = 0;
+            //while(contador< valoresArray.Length)
+            //{
+             //   Console.WriteLine(valoresArray[contador]);
+            //    contador ++;
+            //}
+
+            //foreach(var i in valoresArray)
+            //{
+             //   Console.WriteLine(i);
+            //}
+
+
+            var notas = new List<int> {10, 5, 3, 2, 10, 4, 5, 6, 8,2};
+            var anyNota = notas.Any(n => n == 2);
+            var firstNota10 = notas.First(n => n ==10);
+            var singleNota8 = notas.Single(n => n == 8);
+            var orderedNotas = notas.OrderBy(n => n);
+            var max = notas.Max();
+            var min = notas.Min();
+            var media = notas.Average();
+            var tamanho = notas.Count();
+
+            Console.WriteLine(anyNota);
+            Console.WriteLine(firstNota10);
+            Console.WriteLine(singleNota8);
+
+            foreach(var nota in orderedNotas)
+            {
+                Console.Write( nota + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"Max é {max}, min = {min}, média = {media} e o numero de notas é = {tamanho}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Luis_dev/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Luis_dev")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Luis_dev")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56adf334-2710-4b97-944e-da15c2c3aa5f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
